6f3131de962a50ab821eff6be7daef6d0283e525
JUHI NAGPURE <juhinagpure143@gmail.com>
2025-01-29 18:38:31 +0530

    Update webpack cache and add hot-update files for improved development experience

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8a4553880ab3956d"; }
+/******/ 		__webpack_require__.h = function() { return "58ccba07e59b4d02"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
